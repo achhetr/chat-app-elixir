e1c8557bbb9dd6c348a436db66315e3b9ee40b7e
Akash Chhetri <akash.chhetri@coinjar.com>
2022-08-26 19:14:37 +1000

    form reset added

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -7,11 +7,19 @@ defmodule ChatWeb.RoomLive do
     topic = "room:" <> room_id
     if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
 
-    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Akash joined the chat"], temporary_assigns: [messages: []])}
+    {:ok,
+    assign(socket,
+      room_id: room_id,
+      topic: topic,
+      message: "",
+      messages: [%{uuid: UUID.uuid4(), content: "Elixir joined the chat"}],
+      temporary_assigns: [messages: []]
+    )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    message = %{uuid: UUID.uuid4(), content: message}
     ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
 
     {:noreply, socket}
@@ -19,6 +27,11 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def handle_info(%{event: event, payload: message}, socket) do
-    {:noreply, assign(socket, messages: [message])}
+    {:noreply, assign(socket, messages: "")}
+  end
+
+  @impl true
+  def handle_event("form_update", %{"chat" => %{"message" => message}}, socket) do
+    {:noreply, assign(socket, message: [message])}
   end
 end
